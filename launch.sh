cca59c819895101d5edd46a3845126a383e5f8f7
Andres <andres.medina5@hotmail.com>
2024-06-19 13:53:59 +0200

    edit

--- launch.sh
+++ launch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 cd build && make && cd ..
-rm -r 20*
+
 sleep 3;
 
-gnome-terminal -- bash -c /home/andri/Descargas/rcssserver-19.0.0/src/rcsoccersim & sleep 1
+
 
 
 
 for i in {0..10}
 do
-  gnome-terminal -- bash -c "/home/andri/Escritorio/FC_Oesia/build/player FC_Oesia 555$i" & sleep 1
+  gnome-terminal -- bash -c "/home/Andres5/Escritorio/FC_Oesia/build/player FC_Oesia 555$i" & sleep 1
 done
