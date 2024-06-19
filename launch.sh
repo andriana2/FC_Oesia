da98f203578b7099e6749062ad925c846f417a5a
Andres <andres.medina5@hotmail.com>
2024-06-19 14:18:19 +0200

    edit

--- launch.sh
+++ launch.sh
@@ -14,5 +14,5 @@ sleep 3;
 
 for i in {0..10}
 do
-  gnome-terminal -- bash -c "/home/Andres5/Escritorio/FC_Oesia/build/player FC_Oesia 555$i" & sleep 1
+  gnome-terminal -- bash -c "/home/andres5/Escritorio/FC_Oesia/build/player FC_Oesia 555$i" & sleep 1
 done
