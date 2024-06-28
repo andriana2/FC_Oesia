74d602ae57371dafd600e8abf84018aa7bb4110a
francarg8 <francarg8@gmail.com>
2024-06-28 12:26:58 +0200

    Movientos de jugadores

--- area.cpp
+++ area.cpp
@@ -1,58 +1,95 @@
 
 #include "area.h"
 
-bool area_portero(Datos_Juego &datos)
-{   //defino numero portero
+bool check_area(Datos_Juego &datos)
+{
     bool portero = (datos.jugador.jugador_numero == "1");
+    bool defensa = (datos.jugador.jugador_numero == "2" || datos.jugador.jugador_numero == "3" || datos.jugador.jugador_numero == "4" || datos.jugador.jugador_numero == "5");
+    bool mc = (datos.jugador.jugador_numero == "6" || datos.jugador.jugador_numero == "7" || datos.jugador.jugador_numero == "8");
+    bool delantero = (datos.jugador.jugador_numero == "9" || datos.jugador.jugador_numero == "10" || datos.jugador.jugador_numero == "11");
+
     if (portero)
     {
-        if ((datos.jugador.x_absoluta < 13 && datos.jugador.y_absoluta > 8) || (datos.jugador.y_absoluta < 47 && datos.jugador.y_absoluta > 33)){
+        if ((datos.jugador.x_absoluta < 13 && datos.jugador.y_absoluta > 8) || (datos.jugador.y_absoluta < 47 && datos.jugador.y_absoluta > 33))
+        {
             return true;
         }
     }
 
+    else if (defensa)
+        if ((datos.jugador.x_absoluta < 60 && datos.jugador.x_absoluta > 24) || (datos.jugador.y_absoluta < 74 && datos.jugador.y_absoluta > 6))
+        {
+            return true;
+        }
+
+        else if (mc)
+            if ((datos.jugador.x_absoluta < 90 && datos.jugador.x_absoluta > 34) || (datos.jugador.y_absoluta < 74 && datos.jugador.y_absoluta > 6))
+            {
+                return true;
+            }
+
+            else if (delantero)
+            {
+                if ((datos.jugador.x_absoluta < 112 && datos.jugador.x_absoluta > 50) || (datos.jugador.y_absoluta < 74 && datos.jugador.y_absoluta > 6))
+                {
+                    return true;
+                }
+            }
     return false;
 }
 
-bool area_defensas(Datos_Juego &datos)
-{   //defino numero defensa
+string movimientos_jugador(Datos_Juego &datos)
+{
     bool defensa = (datos.jugador.jugador_numero == "2" || datos.jugador.jugador_numero == "3" || datos.jugador.jugador_numero == "4" || datos.jugador.jugador_numero == "5");
-    
-    if (defensa)
-    {
-        if ((datos.jugador.x_absoluta < 50 && datos.jugador.x_absoluta > 24) || (datos.jugador.y_absoluta < 74 && datos.jugador.y_absoluta > 6)){
-            return true;
-        }
-    }
 
-    return false;
-
-    }
-
-bool area_mcs(Datos_Juego &datos)
-{   //defino numero mediocentros
     bool mc = (datos.jugador.jugador_numero == "6" || datos.jugador.jugador_numero == "7" || datos.jugador.jugador_numero == "8");
-    
-    if (mc)
-    {
-        if ((datos.jugador.x_absoluta < 80 && datos.jugador.x_absoluta > 34) || (datos.jugador.y_absoluta < 74 && datos.jugador.y_absoluta > 6)){
-            return true;
-        }
-    }
-
-    return false;
-}
-
-bool area_delantero(Datos_Juego &datos)
-{   //defino numero delanteros
     bool delantero = (datos.jugador.jugador_numero == "9" || datos.jugador.jugador_numero == "10" || datos.jugador.jugador_numero == "11");
-    
-    if (delantero)
-    {
-        if ((datos.jugador.x_absoluta <112 && datos.jugador.x_absoluta > 50) || (datos.jugador.y_absoluta < 74 && datos.jugador.y_absoluta > 6)){
-            return true;
-        }
-    }
 
-    return false;
+    if (check_area(datos))
+    {
+        if (defensa)
+        {
+            if (stod(datos.ball.balon_distancia) >= 40)
+                return "15";
+            else if (stod(datos.ball.balon_distancia) < 40)
+                return "0";
+            else if (stod(datos.ball.balon_distancia) < 10)
+                return "80";
+            else if (stod(datos.ball.balon_distancia) < 1)
+                return "-1";
+            else
+                return "-1";
+        }
+
+        else if (mc)
+        {
+            if (stod(datos.ball.balon_distancia) >= 30)
+                return "15";
+            else if (stod(datos.ball.balon_distancia) < 30)
+                return "0";
+            else if (stod(datos.ball.balon_distancia) < 10)
+                return "80";
+            else if (stod(datos.ball.balon_distancia) < 1)
+                return "-1";
+            else
+                return "-1";
+        }
+
+        else if (delantero)
+        {
+            if (stod(datos.ball.balon_distancia) >= 25)
+                return "15";
+            else if (stod(datos.ball.balon_distancia) < 25)
+                return "0";
+            else if (stod(datos.ball.balon_distancia) < 15)
+                return "80";
+            else if (stod(datos.ball.balon_distancia) < 1)
+                return "-1";
+            else
+                return "-1";
+        }
+
+        else
+            return "0";
+    }
 }
