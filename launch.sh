dca1f410c636fc095c629a1319a50a12aab33ad4
andriana <97804237+andriana2@users.noreply.github.com>
2024-07-14 11:11:34 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -5,7 +5,9 @@ cd build && make && cd ..
 
 sleep 2;
 
-for i in {0..10}
+gnome-terminal -- bash -c "./build/player FC_Oesia 566$i" & sleep 1
+
+for i in {1..10}
 do
   gnome-terminal -- bash -c "./build/player FC_Oesia 555$i" & sleep 1
 done
